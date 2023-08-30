40c1dfe651abe748434929b808ddc877cdcf852b
guyrawit <guyrawit@hotmail.com>
2023-08-30 16:14:54 +0700

    assignment 4

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,14 +1,15 @@
-import style from './productcard.module.css'
 import Image from 'next/image'
 
 export default function ProductCard({ hospitalName, imgSrc }: { hospitalName: string, imgSrc: string }) {
     return (
         <div className="w-1/5 bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="h-[70%]">
-                <img
+                <Image
                     src={imgSrc}
                     alt="Product Card"
                     className="w-full h-full object-cover rounded-t-lg"
+                    width={600} // Set the desired width
+                    height={400} // Set the desired height
                 />
             </div>
             <div className="p-4">
